edd3cd8451212add6c5ce16b54c9878503ae7b9b
Manish Prasad <manisprasad60@gmail.com>
2025-07-03 02:10:31 +0530

    fix cors issue

--- backend/config/allowedOrigin.ts
+++ backend/config/allowedOrigin.ts
@@ -1,6 +1,7 @@
 export const origins:string[] = [
     "https://www.baseacademy.in/",
     "https://base-academy.vercel.app",
+    "https://base-academy-frontend.vercel.app/login",
     "http://localhost:5173",
     "http://192.168.210.84:5173",
     "https://2ab9-2401-4900-a002-5e17-7045-c53f-3df4-c976.ngrok-free.app"
